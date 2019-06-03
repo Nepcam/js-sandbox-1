62606e83bd478274179d3f74501a73fa22d777f4
Nepcam <cameron@aux.network>
2019-06-03 20:56:26 +1200

    learning to go up and down the DOM

--- traversingTheDOM.js
+++ traversingTheDOM.js
@@ -10,7 +10,7 @@ val = list;
 val = list.childNodes;
 val = list.childNodes[0];
 val = list.childNodes[0].nodeName;
-val = list.childNodes[1].nodeType;
+val = list.childNodes[3].nodeType;
 
 // Node Types
 // 1 - Element
@@ -22,6 +22,36 @@ val = list.childNodes[1].nodeType;
 
 
 // Get children element nodes
-// val = list.children;
+val = list.children;
+val = list.children[0];
+list.children[2].textContent = 'Kia ora';
+
+// Children of children
+list.children[3].children[0].id = 'test=link';
+val = list.children[3].children[0];
+
+// First Child
+val = list.firstChild;
+val = list.firstElementChild;
+
+// Last Child
+val = list.lastChild;
+val = list.lastElementChild;
+
+// Count child elements
+val = list.childElementCount;
+
+// Get parent node
+val = listItem.parentNode;
+val = listItem.parentElement;
+val = listItem.parentElement.parentElement;
+
+// Get next sibling
+val = listItem.nextSibling;
+val = listItem.nextElementSibling.nextElementSibling;
+
+// Get previous sibling
+// val = listItem.previousSibling;
+// val = listItem.previousElementSibling.previousElementSibling;
 
 console.log(val);
