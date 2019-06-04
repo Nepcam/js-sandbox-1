4dc55307b4b598a5335a8205d1c20f7460ab6166
Nepcam <cameron@aux.network>
2019-06-04 21:09:44 +1200

    creating elements

---
+++ createElements.js
@@ -0,0 +1,19 @@
+// Create element
+const li = document.createElement('li');
+
+// Add class
+li.className = 'collection-item';
+
+// Add id
+li.id = 'new-item';
+
+// Add attribute
+li.setAttribute('title', 'New Item');
+
+// Create text node and append
+li.appendChild(document.createTextNode('Hello World'));
+
+// Append li as child to ul
+document.querySelector('ul.collection').appendChild(li);
+
+console.log(li);
