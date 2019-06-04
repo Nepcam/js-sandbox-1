0e2446df2838f9e2855b0b7c71cd6d79cbcb1f57
Nepcam <cameron@aux.network>
2019-06-04 21:57:30 +1200

    creating elements

--- createElements.js
+++ createElements.js
@@ -13,6 +13,16 @@ li.setAttribute('title', 'New Item');
 // Create text node and append
 li.appendChild(document.createTextNode('Hello World'));
 
+// Create new link element
+const link = document.createElement('a');
+// Add classes
+link.className = 'delete-item secondary-content';
+// Add icon html
+link.innerHTML = '<i class="fa fa-remove"></i>';
+
+// Append link into li
+li.appendChild(link);
+
 // Append li as child to ul
 document.querySelector('ul.collection').appendChild(li);
 
