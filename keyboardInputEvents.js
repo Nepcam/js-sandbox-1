64d8a7173038e9bfc688d1c2abcac0fb2f4e3b06
Nepcam <cameron@aux.network>
2019-06-11 21:33:39 +1200

    Input events

--- keyboardInputEvents.js
+++ keyboardInputEvents.js
@@ -1,12 +1,49 @@
 const form = document.querySelector('form');
 const taskInput = document.getElementById('task');
+const heading = document.querySelector('h5');
+const select = document.querySelector('select')
 
-form.addEventListener('submit', runEvent);
+// Clear input
+taskInput.value = ''; // this line clears the form
+
+// form.addEventListener('submit', runEvent); // form with an event listener that passes in submit and the runEvent function 
+
+// //Keydown
+// taskInput.addEventListener('keydown', runEvent);
+
+// //Keyup
+// taskInput.addEventListener('keyup', runEvent);
+
+// //Keypress
+// taskInput.addEventListener('keypress', runEvent);
+
+// //Focus
+// taskInput.addEventListener('focus', runEvent);
+// //Blur
+// taskInput.addEventListener('blur', runEvent);
+// //Cut
+// taskInput.addEventListener('cut', runEvent);
+// //Paste
+// taskInput.addEventListener('paste', runEvent);
+//Input
+// taskInput.addEventListener('input', runEvent);
+
+// Change
+select.addEventListener('change',runEvent);
+
+
+
+
 
 function runEvent(e){
     console.log(`EVENT TYPE: ${e.type}`);
 
-    console.log(taskInput.value);
+    console.log(e.target.value);
 
-    e.preventDefault();
+    // heading.innerText = e.target.value;
+
+//     //Get input value
+//     console.log(taskInput.value);
+
+//     e.preventDefault(); //Usually when you set a submit you may want to have a preventDefault(); inside the function
 }
