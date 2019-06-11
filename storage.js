d28fa03ea9ceb15d0f91ee168f6139722dcd079f
Nepcam <cameron@aux.network>
2019-06-12 11:52:16 +1200

    created an array of tasks and stored it as a string

--- storage.js
+++ storage.js
@@ -20,6 +20,16 @@
 document.querySelector('form').addEventListener('submit', function(e){
     const task = document.getElementById('task').value;
 
+    let tasks;
+
+    if(localStorage.getItem('tasks') === null){
+        tasks = [];
+    } else {
+        tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+
+    tasks.push(task);
+
     localStorage.setItem('task', task);
     alert('Task saved');
 
